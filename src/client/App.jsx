a1a41d39a90b7078d56febd75ca89ec227584837
Robert Mccreary <fooyinfighter@gmail.com>
2022-10-15 11:16:38 -0700

    basic setup for react

---
+++ src/client/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (<button onClick={() => this.setState({liked: true})}>Like</button>)
+  }
+}
+
+const domContainer = document.querySelector('#react_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
